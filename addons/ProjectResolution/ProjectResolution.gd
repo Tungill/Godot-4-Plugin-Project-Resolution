159dca18e2aee2bd02af5eec16b41c7401f73769
S0123n <bessy.i@free.fr>
2024-08-13 18:44:55 +0900

    Update ProjectResolution.gd
    + New button and var boolean to be able to chose to Show/Hide the resolution when it is larger than the current dekstop resolution
    + New resolutions for Steam Deck and Nintendo Switch
    / Updated func _set_resolution(id) to apply the selected resolution to Window Width/Height Override instead of Viewport Width/Height
    / Updated const string to match the change in func _set_resolution(id)
    / Updated "Play" button display names

--- addons/ProjectResolution/ProjectResolution.gd
+++ addons/ProjectResolution/ProjectResolution.gd
@@ -2,8 +2,8 @@
 extends EditorPlugin
 
 const POPUP_BUTTON_TEXT = "Project Resolution"
-const MENU_BUTTON_TOOLTIP = "Quickly change and test different Project Resolution settings"
-const PLUGIN_SELF_NAME = "ProjectResolution" #this variable must match the name of the plugin
+const MENU_BUTTON_TOOLTIP = "Quickly change and test different Game Window Resolution settings"
+const PLUGIN_SELF_NAME = "ProjectResolution" # this variable must match the name of the plugin
 
 var _plugin_menu_btn = MenuButton.new()
 var _plugins_menu = _plugin_menu_btn.get_popup()
@@ -13,6 +13,7 @@ var play_on_change = false
 var play_current_scene = false
 var multistart = false
 var landscape = false
+var hide_larger_resolution = true
 
 func _enter_tree():
 	# Initialization
@@ -52,42 +53,65 @@ func _refresh_plugins_menu_list() -> void:
 func _populate_menu() -> void:
 	var current_fullscreen = ProjectSettings.get_setting("display/window/size/mode")
 #	print("fullscreen status: "+ str(current_fullscreen))
-	var current_res = str(ProjectSettings.get_setting("display/window/size/viewport_width"))+"x"+str(ProjectSettings.get_setting("display/window/size/viewport_height"))
+	var current_res = str(ProjectSettings.get_setting("display/window/size/window_width_override"))+"x"+str(ProjectSettings.get_setting("display/window/size/window_height_override"))
 #	print("current_res: " + current_res)
 	
-	#by default added buttons are unchecked unless true
+	# by default added buttons are unchecked unless true
 	var isPluginEnabled = false
 	
-	#Add Button settings
-	var _buttons = ["Fullscreen", "Play On Change", "Play Current Scene", "Multistart"]
+	# Add Button settings
+	var _buttons = ["Fullscreen", "Play Auto On Change", "Play Auto Current Scene", "Multistart", "Hide Larger Resolution"]
 	for button in _buttons:
 		isPluginEnabled = false
 		if button == "Fullscreen" and current_fullscreen == 3:
 			isPluginEnabled = true
-		if button == "Play On Change" and play_on_change == true:
+		if button == "Play Auto On Change" and play_on_change == true:
 			isPluginEnabled = true
-		if button == "Play Current Scene" and play_current_scene == true:
+		if button == "Play Auto Current Scene" and play_current_scene == true:
 			isPluginEnabled = true
 		if button == "Multistart" and multistart == true:
+			isPluginEnabled = true
+		if button == "Hide Larger Resolution" and hide_larger_resolution == true:
 			isPluginEnabled = true
 
 		_plugins_menu.add_check_item(button)
 		_plugins_menu.set_item_checked(_menu_items_idx, isPluginEnabled)
 		_menu_items_idx += 1
 	
-	#Add desktop resolutions
-	_plugins_menu.add_separator("Desktop");
+	# Add desktop resolutions
+	_plugins_menu.add_separator("DEKSTOP");
 	_menu_items_idx += 1
 	
-	var _resolutions = ["Native", "2560x1600", "2560x1440", "1920x1200", "1920x1080", "1600x1200", "1280x1024", "1280x720", "1024x768", "800x600", "720x405", "640x480", "640x360","320x240"]
+	var _resolutions = ["Native", "2560x1600", "2560x1440", "1920x1200", "1920x1080", "1600x1200", "1280x1024", "1280x720", "1024x768", "800x600", "720x405", "640x480", "640x360","320x240", "0x0"]
 	for resolution in _resolutions:
 		isPluginEnabled = false
 		if resolution == current_res:
 			isPluginEnabled = true
 		
-		#Hide or skip if resolution is larger than desktop resolution
+		# Hide or skip if resolution is larger than desktop resolution
 		if resolution != "Native":
 			var res = resolution.split("x")
+			if hide_larger_resolution == true:
+				if int(res[0]) > DisplayServer.screen_get_size().x or int(res[1]) > DisplayServer.screen_get_size().y:
+					continue
+		
+		_plugins_menu.add_check_item(resolution)
+		_plugins_menu.set_item_checked(_menu_items_idx, isPluginEnabled)
+		_menu_items_idx += 1
+	
+	# Add widescreen resolutions
+	_plugins_menu.add_separator("WIDESCREEN");
+	_menu_items_idx += 1
+	
+	var _widescreen_resolutions = ["5120x1440", "3840x2160", "3840x1080", "3440x1440", "2560x1080", "2560x720", "1920x540", "1280x360"]
+	for resolution in _widescreen_resolutions:
+		isPluginEnabled = false
+		if resolution == current_res:
+			isPluginEnabled = true
+		
+		# Hide or skip if resolution is larger than desktop resolution
+		var res = resolution.split("x")
+		if hide_larger_resolution == true:
 			if int(res[0]) > DisplayServer.screen_get_size().x or int(res[1]) > DisplayServer.screen_get_size().y:
 				continue
 		
@@ -95,27 +119,48 @@ func _populate_menu() -> void:
 		_plugins_menu.set_item_checked(_menu_items_idx, isPluginEnabled)
 		_menu_items_idx += 1
 	
-	#Add widescreen resolutions
-	_plugins_menu.add_separator("Widescreen");
+	# Add handheld console resolutions
+	_plugins_menu.add_separator("HANDLED CONSOLE");
 	_menu_items_idx += 1
 	
-	var _widescreen_resolutions = ["5120x1440", "3840x1080", "2560x1080", "2560x720", "1920x540", "1280x360"]
-	for resolution in _widescreen_resolutions:
+	_plugins_menu.add_separator("Steam Deck");
+	_menu_items_idx += 1
+	var _steam_deck_resolutions = ["1280x800"]
+	for resolution in _steam_deck_resolutions:
 		isPluginEnabled = false
 		if resolution == current_res:
 			isPluginEnabled = true
 		
-		#Hide or skip if resolution is larger than desktop resolution
+		# Hide or skip if resolution is larger than desktop resolution
 		var res = resolution.split("x")
-		if int(res[0]) > DisplayServer.screen_get_size().x or int(res[1]) > DisplayServer.screen_get_size().y:
-			continue
+		if hide_larger_resolution == true:
+			if int(res[0]) > DisplayServer.screen_get_size().x or int(res[1]) > DisplayServer.screen_get_size().y:
+				continue
 		
 		_plugins_menu.add_check_item(resolution)
 		_plugins_menu.set_item_checked(_menu_items_idx, isPluginEnabled)
 		_menu_items_idx += 1
 	
-	#Add mobile resolutions
-	_plugins_menu.add_separator("Mobile");
+	_plugins_menu.add_separator("Nintendo Switch");
+	_menu_items_idx += 1
+	var _nintendo_switch_resolutions = [ "1280x720"]
+	for resolution in _nintendo_switch_resolutions:
+		isPluginEnabled = false
+		if resolution == current_res:
+			isPluginEnabled = true
+		
+		# Hide or skip if resolution is larger than desktop resolution
+		var res = resolution.split("x")
+		if hide_larger_resolution == true:
+			if int(res[0]) > DisplayServer.screen_get_size().x or int(res[1]) > DisplayServer.screen_get_size().y:
+				continue
+		
+		_plugins_menu.add_check_item(resolution)
+		_plugins_menu.set_item_checked(_menu_items_idx, isPluginEnabled)
+		_menu_items_idx += 1
+	
+	# Add mobile resolutions
+	_plugins_menu.add_separator("MOBILE");
 	_menu_items_idx += 1
 	
 	var buttonLandscape = "Landscape"
@@ -134,10 +179,11 @@ func _populate_menu() -> void:
 			var res = resolution.split("x")
 			resolution = res[1]+"x"+res[0]
 		
-		#Hide or skip if resolution is larger than desktop resolution
+		# Hide or skip if resolution is larger than desktop resolution
 		var res = resolution.split("x")
-		if int(res[0]) > DisplayServer.screen_get_size().x or int(res[1]) > DisplayServer.screen_get_size().y:
-			continue
+		if hide_larger_resolution == true:
+			if int(res[0]) > DisplayServer.screen_get_size().x or int(res[1]) > DisplayServer.screen_get_size().y:
+				continue
 		
 		_plugins_menu.add_check_item(resolution)
 		_plugins_menu.set_item_checked(_menu_items_idx, isPluginEnabled)
@@ -146,42 +192,45 @@ func _populate_menu() -> void:
 
 func _set_resolution(id) -> void:
 	var is_item_checked = _plugins_menu.is_item_checked(id)
-#	print(str(id) + " , " + _plugins_menu.get_item_text(id) + " , " + str(is_item_checked))
+	#print(str(id) + " , " + _plugins_menu.get_item_text(id) + " , " + str(is_item_checked))
 	
 	_plugins_menu.set_item_checked(id, not is_item_checked)
 	
 	var item_name = _plugins_menu.get_item_text(id)
 	if item_name == "Fullscreen":
-		#Set Project Fullscreen setting
+		# Set Project Fullscreen setting
 		var current_win = ProjectSettings.get_setting("display/window/size/mode")
 		if current_win == 0:
 			ProjectSettings.set_setting("display/window/size/mode", 3)
 		else:
 			ProjectSettings.set_setting("display/window/size/mode", 0)
 		print("Fullscreen: "+str(ProjectSettings.get_setting("display/window/size/mode")))
-	elif item_name == "Play On Change":
+	elif item_name == "Play Auto On Change":
 		play_on_change = !play_on_change
-		print("Play On Change: "+str(play_on_change))
-	elif item_name == "Play Current Scene":
+		print("Play Auto On Change: "+str(play_on_change))
+	elif item_name == "Play Auto Current Scene":
 		play_current_scene = !play_current_scene
-		print("Play Current Scene: "+str(play_current_scene))
+		print("Play Auto Current Scene: "+str(play_current_scene))
 	elif item_name == "Multistart":
 		multistart = !multistart
 		print("Multistart: "+str(multistart))
 	elif item_name == "Landscape":
 		landscape = !landscape
 		print("Landscape: "+str(landscape))
+	elif item_name == "Hide Larger Resolution":
+		hide_larger_resolution = !hide_larger_resolution
+		print("Hide Larger Resolution: "+str(hide_larger_resolution))
 	else:
-		#Set Project Resolution settings
+		# Set Project Resolution settings
 		if item_name == "Native":
 			item_name = str(DisplayServer.screen_get_size().x) + "x" + str(DisplayServer.screen_get_size().y)
 		
 		var res = item_name.split("x")
-		ProjectSettings.set_setting("display/window/size/viewport_width", res[0])
-		ProjectSettings.set_setting("display/window/size/viewport_height", res[1])
-		print("Set Project Resolution: " + str(ProjectSettings.get_setting("display/window/size/viewport_width"))+"x"+str(ProjectSettings.get_setting("display/window/size/viewport_height")))
+		ProjectSettings.set_setting("display/window/size/window_width_override", res[0])
+		ProjectSettings.set_setting("display/window/size/window_height_override", res[1])
+		print("Set Project Resolution: " + str(ProjectSettings.get_setting("display/window/size/window_width_override"))+"x"+str(ProjectSettings.get_setting("display/window/size/window_height_override")))
 		
-		#Update menu button name by resolution
+		# Update menu button name by resolution
 		_plugin_menu_btn.text = item_name
 		
 		if play_on_change and !play_current_scene and !multistart:
@@ -190,12 +239,12 @@ func _set_resolution(id) -> void:
 			get_editor_interface().play_current_scene()
 		else:
 			if multistart:
-				#if multistart is true, set to window mode instead of fullscreen
+				# if multistart is true, set to window mode instead of fullscreen
 				ProjectSettings.set_setting("display/window/size/mode", 0)
 				#if multistart is true, set play_current_scene to false
 				play_current_scene = false
 		if multistart and ProjectSettings.get_setting("display/window/size/mode") == 0:
-			#start 2 window instance
+			# start 2 window instance
 			var spacing = 2
 			var window_pos = Vector2(int(DisplayServer.screen_get_size().x)/2 - int(res[0])-spacing, int(DisplayServer.screen_get_size().y)/2 - int(res[1])/2)
 			OS.create_process(OS.get_executable_path(), ["--path", ".", "--position", window_pos, false])
